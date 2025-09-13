028c0279091fa59677724510318641d7220de6bf
SashaDyka <beli.rothi2005@gmail.com>
2025-09-13 18:27:47 +0300

    add service mrthods skeleton

--- src/bills/bills.service.ts
+++ src/bills/bills.service.ts
@@ -5,12 +5,30 @@ import { PrismaServise } from '../prisma/prisma.service';
 export class BillsService{
     constructor(private prisma: PrismaServise){}
 
+    //TODO: create calculateBillResults method
+
+    async createBill(data: any){
+
+    }
+
     async findAll(){
         return this.prisma.bill.findMany({
             include: {
                 people: true,
             },
         });
+    }
+
+    async findOne(id: number){
+
+    }
+
+    async update(id: number, data: any){
+
+    }
+
+    async remove(id: number){
+
     }
 
 }
