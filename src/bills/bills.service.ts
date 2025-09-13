88dc442f2715992c99768e65c90772f4fada56e6
SashaDyka <beli.rothi2005@gmail.com>
2025-09-13 22:49:00 +0300

    add method create bill

--- src/bills/bills.service.ts
+++ src/bills/bills.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaServise } from '../prisma/prisma.service';
+import { CreateBillDto } from '../dto/create-bill.dto';
+
 
 @Injectable() 
 export class BillsService{
@@ -7,9 +9,32 @@ export class BillsService{
 
     //TODO: create calculateBillResults method
 
-    async createBill(data: any){
+    async createBill(data: CreateBillDto){
+        const {totalAmount, defaultTipPercentage, people} = data;
+        if(totalAmount <= 0){
+            throw new BadRequestException('Total amount must be a positive number.');
+        }
+        const totalIndividualAmount = people
+            .filter(p => p.individualAmount !== undefined && p.individualAmount !== null)
+            .reduce((sum, p) => sum + p.individualAmount, 0);
 
+        if (totalIndividualAmount > totalAmount) {
+            throw new BadRequestException('The sum of individual payments cannot exceed the total amount.');
+        }
+
+        const prismaData = {
+            totalAmount,
+            defaultTipPercentage,
+            people: {
+                create: people,
+            },
+        };
+        
+        return this.prisma.bill.create({data: prismaData});
     }
+
+
+    
 
     async findAll(){
         return this.prisma.bill.findMany({
