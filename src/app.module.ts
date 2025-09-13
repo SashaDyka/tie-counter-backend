5a899f655094bacb53c8a70d133630e4b7a209ee
SashaDyka <beli.rothi2005@gmail.com>
2025-09-13 14:00:13 +0300

    import BillsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BillModele } from './bills/bills.model';
 
 @Module({
-  imports: [],
+  imports: [BillModele],
   controllers: [AppController],
   providers: [AppService],
 })
