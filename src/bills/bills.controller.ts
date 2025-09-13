6e2b6e07edf92224b3e0836aefb99b6d6d153469
SashaDyka <beli.rothi2005@gmail.com>
2025-09-13 18:28:27 +0300

    add controller methods skeleton

--- src/bills/bills.controller.ts
+++ src/bills/bills.controller.ts
@@ -5,9 +5,30 @@ import { BillsService } from './bills.service';
 export class BillsController{
     constructor(private readonly billsService: BillsService) {}
 
+    @Post()
+    create(@Body() createBillDto: any){
+        return this.billsService.createBill(createBillDto); //TODO: create Bill DTO
+    }
+
     @Get()
     findAll(){
         return this.billsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string){
+        return this.billsService.findOne(+id);
+    }    
+
+
+    @Patch()
+    update(){
+        return 
+    }
+
+    @Delete()
+    remove(){
+        return
     }
 
 
