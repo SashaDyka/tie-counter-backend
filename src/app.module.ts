46fdaf3d17057c8068291d48c10b4f33b29e80dd
SashaDyka <beli.rothi2005@gmail.com>
2025-09-13 18:28:59 +0300

    connect the persons model

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BillModele } from './bills/bills.model';
+import { PersonsModel } from './persons/persons.model';
+
 
 @Module({
-  imports: [BillModele],
+  imports: [BillModele, PersonsModel],
   controllers: [AppController],
   providers: [AppService],
 })
