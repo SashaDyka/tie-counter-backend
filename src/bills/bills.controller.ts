41dd086a257658d7292594d5912a9e538ca6fcc2
SashaDyka <beli.rothi2005@gmail.com>
2025-09-13 23:22:28 +0300

    edit methods create, remove and update

--- src/bills/bills.controller.ts
+++ src/bills/bills.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get, Post, Patch, Body, Param, Delete } from '@nestjs/common';
 import { BillsService } from './bills.service';
+import { CreateBillDto } from '../dto/create-bill.dto';
+
+
 
 @Controller('bills')
 export class BillsController{
     constructor(private readonly billsService: BillsService) {}
 
     @Post()
-    create(@Body() createBillDto: any){
-        return this.billsService.createBill(createBillDto); //TODO: create Bill DTO
+    create(@Body() createBillDto: CreateBillDto){
+        return this.billsService.createBill(createBillDto);
     }
 
     @Get()
@@ -21,14 +24,15 @@ export class BillsController{
     }    
 
 
-    @Patch()
-    update(){
-        return 
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateBillDto: KLKMKL) {
+        return this.billsService.update(+id, updateBillDto);
     }
 
-    @Delete()
-    remove(){
-        return
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.billsService.remove(+id);
     }
 
 
